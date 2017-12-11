91afa16c37632f3c2c2a08bfe0095cc50ffe6619
aliciapflaumer <alpfla7@gmail.com>
2017-12-11 13:57:51 -0500

    Change to `quizzes_controller`
    - A `user` can create a quiz, a quiz belongs_to a `user`
    
    - Currently quizzes can be viewed, edited, and deleted by others
    (need to fix in future iteration)

--- scripts/quiz/create.sh
+++ scripts/quiz/create.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#TOKEN=BAhJIiUwZjY1OWY5MTkwZGFjNTFhYjQ5ODBkM2I4YzUwNTRlZgY6BkVG--c125d46d3762b57166676cf35e063fd5ca4e60d5
 API="http://localhost:4741"
 URL_PATH="/quizzes"
 curl "${API}${URL_PATH}" \
